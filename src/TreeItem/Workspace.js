151fb78b8079d663220e03d8681ec1fad98f3969
Christoph Erdmann <git@cerdmann.com>
2022-10-18 20:11:15 +0200

    style: lint Workspace.js

--- src/TreeItem/Workspace.js
+++ src/TreeItem/Workspace.js
@@ -3,35 +3,35 @@ const path      = require('path');
 const helper    = require ('../helpers');
 
 module.exports = class Workspace {
-    constructor(uri, tabGroupIndex, parent, packageData) {
-        const config            = vscode.workspace.getConfiguration("betterOpenEditors");
+	constructor(uri, tabGroupIndex, parent, packageData) {
+		const config            = vscode.workspace.getConfiguration('betterOpenEditors');
 
-        this.contextValue       = "workspace";
-        this.id                 = `${tabGroupIndex}-${uri.path}`;
-        this.resourceUri        = uri;
-        this.path               = helper.normalizePath(uri.path);
-        this.sortKey            = helper.normalizePath(uri.path).toLowerCase();
-        this.collapsibleState   = vscode.TreeItemCollapsibleState.Expanded;
-        
-        this.parent             = parent;
-        this.children           = [];
+		this.contextValue       = 'workspace';
+		this.id                 = `${tabGroupIndex}-${uri.path}`;
+		this.resourceUri        = uri;
+		this.path               = helper.normalizePath(uri.path);
+		this.sortKey            = helper.normalizePath(uri.path).toLowerCase();
+		this.collapsibleState   = vscode.TreeItemCollapsibleState.Expanded;
+		
+		this.parent             = parent;
+		this.children           = [];
 
-        this.label              = path.basename(this.path);
+		this.label              = path.basename(this.path);
 
-        if (packageData) {
-            if (config.get("ShowPackageInfo")) {
-                this.description = `${packageData.name} ${packageData.version}`;
+		if (packageData) {
+			if (config.get('ShowPackageInfo')) {
+				this.description = `${packageData.name} ${packageData.version}`;
 
-                if (config.get("HidePackagePath")) {
-                    this.label = "";
-                }
-            }
-        }
+				if (config.get('HidePackagePath')) {
+					this.label = '';
+				}
+			}
+		}
 
-        if (config.get("ShowWorkspaceIcon")) {
-            this.iconPath = new vscode.ThemeIcon("root-folder");
-        } else {
-            delete this.iconPath;
-        }
-    }
+		if (config.get('ShowWorkspaceIcon')) {
+			this.iconPath = new vscode.ThemeIcon('root-folder');
+		} else {
+			delete this.iconPath;
+		}
+	}
 }
